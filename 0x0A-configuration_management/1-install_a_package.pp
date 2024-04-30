36281b49bc553c1c805e44d3c775d1c8110b78ca
Idongesit Ekanem <aidisapp94@gmail.com>
2024-04-30 15:37:57 +0100

    Update on Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,14 @@
-#!/usr/bin/pup
+#!/usr/bin/env puppet
 # Install flask from pip3.
 # Requirements:
 #   We will need to install flask
 #   The version must be 2.1.0
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
-  ensure   => 2.1.0,
+  ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
