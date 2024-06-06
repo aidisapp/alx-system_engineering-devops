0d25e704c16d12fdad12adfec1a1b42ba17e3523
Idongesit Ekanem <aidisapp94@gmail.com>
2024-06-06 23:23:26 +0100

    Task 0 Changes syntax

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,38 +13,22 @@ import sys
 
 REST_API = "https://jsonplaceholder.typicode.com"
 
-
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
-            employee_id = int(sys.argv[1])
-
-            # Get the employee information
-            user_url = f'{REST_API}/users/{employee_id}'
-            user_response = requests.get(user_url).json()
-
-            # Get the to-do list for the employee
-            todos_url = f'{REST_API}/todos'
-            todos_response = requests.get(todos_url).json()
-
-            # Extract the employee's name
-            employee_name = user_response.get('name')
-
-            # Filter tasks for the specific employee
-            tasks = list(filter(
-                            lambda x: x.get(
-                                    'userId') == employee_id, todos_response))
+            id = int(sys.argv[1])
+            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            task_req = requests.get('{}/todos'.format(REST_API)).json()
+            emp_name = req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
             completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-
-            # Print the employee's name and the number of completed tasks
-            print(f"Employee {employee_name} is done with tasks(
-                                {len(completed_tasks)}/{len(tasks)}): ")
-
-            # Print each completed task
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(completed_tasks),
+                    len(tasks)
+                )
+            )
             if len(completed_tasks) > 0:
                 for task in completed_tasks:
-                    print(f"\t {task.get('title')}")
-        else:
-            print("Please provide a valid employee ID.")
-    else:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+                    print('\t {}'.format(task.get('title')))
