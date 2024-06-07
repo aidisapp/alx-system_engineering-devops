e02c7a0558a90a61c72c8739cadf51a2986a2b4b
Idongesit Ekanem <aidisapp94@gmail.com>
2024-06-08 00:18:27 +0100

    Task 0 Solution

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines a function that interacts with the Reddit API to get the
+number of subscribers for a specified subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Fetches the number of subscribers for a given
+    subreddit using the Reddit API.
+    Returns 0 if the subreddit is invalid.
+
+    Args:
+    subreddit (str): The subreddit to retrieve subscriber count for.
+
+    Returns:
+    int: Number of subscribers, or 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "my-app/0.0.1"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
