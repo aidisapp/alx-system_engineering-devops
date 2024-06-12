5a275ed540f8e66ee94436f98268f1e6f9669c0d
Idongesit Ekanem <84503311+aidisapp@users.noreply.github.com>
2024-06-12 11:34:39 +0100

    Update 0-subs.py
    Error fixes and syntax changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,14 +19,12 @@ def number_of_subscribers(subreddit):
     Returns:
     int: Number of subscribers, or 0 if the subreddit is invalid.
     """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
     else:
         return 0
